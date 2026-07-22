ad9bc6481d784dd91a2c050cf548fa6c2bf21afc
Soyibjon <toychoqse@gmail.com>
2026-07-22 21:20:05 +0500

    fix IsAdmin filter for CallbackQuery

--- filters/admin_filter.py
+++ filters/admin_filter.py
@@ -1,7 +1,11 @@
 from aiogram.filters import BaseFilter
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from config import ADMIN_IDS
 
 class IsAdmin(BaseFilter):
-    async def __call__(self, message: Message) -> bool:
-        return message.from_user.id in ADMIN_IDS
+    async def __call__(self, event) -> bool:
+        if isinstance(event, Message):
+            return event.from_user.id in ADMIN_IDS
+        if isinstance(event, CallbackQuery):
+            return event.from_user.id in ADMIN_IDS
+        return False
